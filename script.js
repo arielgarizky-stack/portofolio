d8fcae3fc1a4ad072cea47be4ca91587a8df65ad
arielgarizky-stack <arielgarizky@gmail.com>
2025-11-28 18:17:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const btn = document.getElementById('toggleTheme');
 btn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+  document.addEventListener("heder", function(e) {
+    e.preventDefault();
+  });
+
